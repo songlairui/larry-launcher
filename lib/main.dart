2b70634513bcf340c4ee8c0f5c254da0aa891cce
songlairui <cnre@live.cn>
2019-05-01 10:38:27 +0800

    使用prefs缓存应用列表

--- lib/main.dart
+++ lib/main.dart
@@ -1,98 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:device_apps/device_apps.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:android_intent/android_intent.dart';
+import 'dart:convert';
+import './app_item.dart';
 
 void main() => runApp(new MyApp());
-
-class AppItem extends StatefulWidget {
-  AppItem({@required this.apps});
-
-  final apps;
-
-  @override
-  ItemState createState() => ItemState(apps: apps);
-}
-
-class ItemState extends State<AppItem> {
-  ItemState({@required this.apps});
-
-  final apps;
-
-//  var icon;
-
-  @override
-  initState() async {
-    super.initState();
-
-//    setState(() {
-////      _installedApps.addAll(apps);
-//    });
-//    DeviceApps.getInstalledApplications(
-//            includeAppIcons: false,
-//            onlyAppsWithLaunchIntent: true,
-//            includeSystemApps: true)
-//        .then((apps) {
-//      setState(() {
-//        _installedApps.addAll(apps);
-//      });
-//    });
-  }
-
-  Widget _buildRow(Application pkgInfo) {
-    return GestureDetector(
-      onTap: () async {
-        print(pkgInfo);
-        print(pkgInfo.appName);
-//        print(pkgInfo.icon);
-        DeviceApps.openApp(pkgInfo.packageName);
-//        ApplicationWithIcon curApp =
-//            await DeviceApps.getApp(pkgInfo.packageName, true);
-//
-//        print('set');
-//        print(curApp.icon);
-      },
-      child: Container(
-        child: new Column(children: [
-//          icon == null
-//              ? null
-//              : new Image.memory(
-//                  icon,
-//                  fit: BoxFit.scaleDown,
-//                  width: 32,
-//                ),
-          new Text(
-            pkgInfo.appName,
-            style: new TextStyle(
-              color: Colors.white,
-              fontSize: 12.0,
-              fontWeight: FontWeight.w900,
-              fontFamily: "Georgia",
-            ),
-          ),
-        ]),
-        color: Colors.black26,
-      ),
-    );
-  }
-
-  Widget _buildAppList() {
-    print('apps');
-    print(apps);
-    if (apps == null) {
-      return new Text("waiting");
-    }
-    return new GridView.count(
-        crossAxisCount: 5,
-        childAspectRatio: 0.9,
-        children: new List.generate(apps.length, (i) {
-          return _buildRow(apps[i]);
-        }));
-  }
-
-  @override
-  Widget build(BuildContext ctx) {
-    return _buildAppList();
-  }
-}
 
 class MyApp extends StatefulWidget {
   @override
@@ -100,22 +13,60 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  SharedPreferences prefs;
+
   var numberOfInstalledApps;
-  List<Application> apps = <Application>[];
+  List apps = [];
   var wallpaper;
 
   @override
-  initState() async {
+  initState() {
     super.initState();
-
-    List<Application> allApps = await DeviceApps.getInstalledApplications(
-        includeSystemApps: true, onlyAppsWithLaunchIntent: true);
-    setState(() {
-      apps.addAll(allApps);
+    SharedPreferences.getInstance().then((ins) {
+      prefs = ins;
+      _loadAllAppFromPref().then((jsonApps) {
+        if (jsonApps == null) {
+          return this._storeAllAppToPref();
+        }
+        return jsonApps;
+      }).then((jsonApps) {
+        setState(() {
+          this.apps.addAll(jsonApps);
+        });
+      });
     });
   }
 
-  _goMiHome() async {}
+  _goMiHome() async {
+    AndroidIntent intent = AndroidIntent(
+        action: 'android.intent.action.MAIN',
+        category: 'android.intent.category.HOME',
+        package: 'com.miui.home');
+    await intent.launch();
+  }
+
+  _loadAllAppFromPref() async {
+    var rawList = prefs.getString('appList');
+    if (rawList == null) {
+      return null;
+    }
+    List jsonApps = jsonDecode(rawList);
+    if (jsonApps.length == 0) {
+      return null;
+    }
+    return jsonApps;
+  }
+
+  _storeAllAppToPref() async {
+    List<Application> allApps = await DeviceApps.getInstalledApplications(
+        includeSystemApps: true, onlyAppsWithLaunchIntent: true);
+    List<Map> json = [];
+    allApps.forEach((app) {
+      json.add({'pkg': app.packageName, 'title': app.appName});
+    });
+    await prefs.setString('appList', jsonEncode(json));
+    return json;
+  }
 
   @override
   Widget build(BuildContext ctx) {
